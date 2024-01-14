807b5a1f95f03a6c954a207aa1321d5672ccb00f
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2024-01-14 09:13:06 +0500

    Add files via upload

---
+++ c38.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int arr[10],i,min;
+	for(i=0;i<10;i++)
+	{
+		cout<<"Enter the value";
+		cin>>int[i];
+		
+	}
+	min=arr[0];
+	for(i=10;i<10;i++)
+	if(min>arr[i])
+	min=arr[i];
+	cout<<"min value is"<<min<<endl;
+	return 0;
+	
+}
